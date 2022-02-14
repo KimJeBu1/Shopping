4cf57a8bb0298d9d844f8afe5a7040ec11f550d6
KimJeBu1 <95723051+KimJeBu1@users.noreply.github.com>
2022-02-14 17:59:56 +0900

    Main: list name compensate( undefine)

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-// 'static'
+'use strict'
 const items = document.querySelector('.items');
 const input = document.querySelector('.footer__input');
 const addBtn = document.querySelector('.footer__button');
@@ -12,10 +12,10 @@ function onAdd() {
         return;
     }
     //2. 새로운 아이템을 만듬(텍스트 + 삭제 버튼)
-    const item = createItem();
+    const item = createItem(text);
     //3. items 컨테이너안에 새로 만든 아이템을 추가한다
     items.appendChild(item);
-    // 4. 새로 추가된 아이템으로 스크롤링
+    //4. 새로 추가된 아이템으로 스크롤링
     item.scrollIntoView({block: 'center'});
     //5. 인풋을 초기화 한다
     input.value = '';
@@ -30,7 +30,7 @@ function createItem(text) {
     item.setAttribute('class', 'item');
 
     const name = document.createElement('span');
-    name.setAttribute('class', 'item__name')
+    name.setAttribute('class', 'item__name');
     name.innerText = text;
     //const item = createItem(); 여기서 text를 creatItem에 전달해주고 그 text를 이용해서
     //item__name에 innerText에 설정
